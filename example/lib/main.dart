52f90b5277b9bf49130eb5a7e10f4ee9c81e2740
omar <omar.alshyokh@gmail.com>
2020-12-28 01:56:28 +0200

    init first commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,22 +23,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Fullscreen image example'),
     );
   }
 }
@@ -46,14 +34,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -62,56 +42,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
         child: SingleChildScrollView(
           child: Container(
             child: Column(
-              // Column is also a layout widget. It takes a list of children and
-              // arranges them vertically. By default, it sizes itself to fit its
-              // children horizontally, and tries to be as tall as its parent.
-              //
-              // Invoke "debug painting" (press "p" in the console, choose the
-              // "Toggle Debug Paint" action from the Flutter Inspector in Android
-              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-              // to see the wireframe for each widget.
-              //
-              // Column has various properties to control how it sizes itself and
-              // how it positions its children. Here we use mainAxisAlignment to
-              // center the children vertically; the main axis here is the vertical
-              // axis because Columns are vertical (the cross axis would be
-              // horizontal).
+
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 SizedBox(height: 20,),
+
                 Text('Image Assets Popup',
                   style: TextStyle(
                       color: Colors.blue,
@@ -119,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),),
                 ImageAssetsFullscreen(
                   imagePath: 'assets/default_image.png',
-                  imageBorderRadius: 20,
+                  imageBorderRadius: 200,
                   imageWidth: 200,
                   imageHeight: 200,
                   iconBackButtonColor: Colors.red,
@@ -130,45 +80,13 @@ class _MyHomePageState extends State<MyHomePage> {
                   backgroundColorDetails: Colors.green,
                   imageDetailsFit: BoxFit.cover,
                   hideAppBarDetails: true,
-                  imageFit: BoxFit.contain,
+                  imageFit: BoxFit.fill,
                   withHeroAnimation: true,
                 ),
 
-
-
-                SizedBox(height: 20,),
-
-                Text('Image Cached Popup',
-                  style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 18
-                  ),),
-                ImageCachedFullscreen(
-                  imageUrl: 'https://picsum.photos/iwd/10011/200/',
-                  imageBorderRadius: 20,
-                  imageWidth: 200,
-                  imageHeight: 200,
-                  placeholder: Container(
-                    child: Icon(Icons.check),
-                  ),
-                  errorWidget: Container(
-                    child: Icon(Icons.error),
-                  ),
+                Divider(
+                  height: 25,
                 ),
-                ImageCachedFullscreen(
-                  imageUrl: 'https://picsum.photos/id/1/200/300',
-                  imageBorderRadius: 20,
-                  imageWidth: 200,
-                  imageHeight: 200,
-                  placeholder: Container(
-                    child: Icon(Icons.check),
-                  ),
-                  errorWidget: Container(
-                    child: Icon(Icons.error),
-                  ),
-                ),
-
-                SizedBox(height: 20,),
 
 
                 Text('Image Network Popup',
@@ -183,17 +101,70 @@ class _MyHomePageState extends State<MyHomePage> {
                   imageHeight: 200,
                 ),
 
+                Divider(
+                  height: 25,
+                ),
+
+
+                Text('Image Cached Popup',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 18
+                  ),),
+                ImageCachedFullscreen(
+                  imageUrl: 'https://picsum.photos/200/300',
+                  imageBorderRadius: 20,
+                  imageWidth: 200,
+                  imageHeight: 200,
+                  withHeroAnimation: true,
+                  placeholder: Container(
+                    child: Icon(Icons.check),
+                  ),
+                  errorWidget: Container(
+                    child: Icon(Icons.error),
+                  ),
+                ),
+
+                Divider(
+                  height: 25,
+                ),
+                Text('Image Cached Popup with custom options',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 18
+                  ),),
+                ImageCachedFullscreen(
+                  imageUrl: 'https://picsum.phots/200/300',
+                  imageBorderRadius: 20,
+                  imageWidth: 200,
+                  imageHeight: 200,
+                  placeholder: Container(
+                    child: Icon(Icons.check),
+                  ),
+                  errorWidget: Container(
+                    child: Icon(Icons.error),
+                  ),
+                  iconBackButtonColor: Colors.red,
+                  imageDetailsHeight: 500,
+                  imageDetailsWidth: 200,
+                  hideBackButtonDetails: true,
+                  appBarBackgroundColorDetails: Colors.red,
+                  backgroundColorDetails: Colors.green,
+                  imageDetailsFit: BoxFit.cover,
+                  hideAppBarDetails: true,
+                  imageFit: BoxFit.fill,
+                  withHeroAnimation: true,
+                ),
+
+                SizedBox(height: 20,),
+
+
+
 
               ],
             ),
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      ),);
   }
 }
